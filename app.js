52c913a7b07ac55a8d6a05023344181e81c0b34c
Pushp Jain <pushp.ug20@nsut.ac.in>
2021-08-03 17:13:31 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const inputDate = document.querySelector("#input-date");
+const inputNumber = document.querySelector("#input-number");
+const checkButton = document.querySelector("#input-button");
+

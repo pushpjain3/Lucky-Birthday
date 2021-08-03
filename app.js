02fc8cd04a673cfd032855201f492b1973ded6a8
Pushp Jain <pushp.ug20@nsut.ac.in>
2021-08-03 23:49:22 +0530

    done with styling

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const inputDate = document.querySelector("#input-date");
 const inputNumber = document.querySelector("#input-number");
 const checkButton = document.querySelector("#input-button");
 const errMsg = document.querySelector(".err");
+const resMsg = document.querySelector(".result-text");
 
 const dateProcessor = (date) => date.replaceAll("-", "");
 const dateSum = (date) => {
@@ -27,7 +28,9 @@ const clickHandler = () => {
 		errMsg.innerText = "";
 		const birthdate = dateProcessor(inputDate.value);
 		if (luckyDateChecker(Number(luckynumber), Number(birthdate))) {
+			resMsg.innerText = "Congratulations You have a Lucky Birthday!!";
 		} else {
+			resMsg.innerText = "Ah! You don't have a Lucky Birthday!!";
 		}
 	} else {
 		errMsg.innerText = "Please enter Lucky number in digits.";
